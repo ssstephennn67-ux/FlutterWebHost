03999a45725dc9d1f12dd8af0f34d4d41cc781d5
ssstephennn67-ux <ssstephennn67-ux@users.noreply.github.com>
2026-01-30 04:18:21 +0000

    Deploying to webhost-pages from @ ssstephennn67-ux/gobblet_gollers@48fa55d3db0b18b114e23f1f7dc94ddf9f862ae6 🚀

--- GobbletGollersWeb/flutter_bootstrap.js
+++ GobbletGollersWeb/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4094656564"
+    serviceWorkerVersion: "1855869548"
   }
 });
