00940683ba6e1b542a582ad7a16596f3301d1e0e
ssstephennn67-ux <ssstephennn67@gmail.com>
2026-07-29 11:44:06 +0000

    Deploying to webhost-pages from @ ssstephennn67-ux/SlidingPuzzle@8c358bce0f2063a6a106326bf85be7232b4a1f78 🚀

--- SlidingPuzzleWeb/flutter_bootstrap.js
+++ SlidingPuzzleWeb/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3444071557" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2701794069" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
